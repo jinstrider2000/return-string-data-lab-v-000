22ca036b7f8dabf6a051e68a8f5b919a21c5abf5
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-22 20:19:50 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   get 'home', to: 'static#home'
   get '/products/:id/description', to: 'products#description'
   get '/products/:id/inventory', to: 'products#inventory'
+  get '/products/:id/truncated-description', to: 'products#truncated_description'
   resources :products, only: [:new, :create, :index]
   resources :orders
   resources :invoices
