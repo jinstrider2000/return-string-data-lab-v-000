be8b0a367a39d8a0e2d36b61145f17c42ee79efb
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-22 18:20:39 +0000

    automatically backed up by learn

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,9 @@
 class Product < ActiveRecord::Base
+  attr_accessor :description, :inventory
   has_many :ordered_products
   has_many :orders, :through => :ordered_products
+
+  def inventory_available?
+    self.inventory > 0 ? true : false
+  end
 end
