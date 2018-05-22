d6aeedf0a9b5cfa7b3f87b9978766017720703b4
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-22 19:15:04 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,12 +1,12 @@
 class ProductsController < ApplicationController
   def description
     prod = Product.find_by(id: params[:id])
-    render :plain, prod.description
+    render plain: prod.description
   end
 
   def inventory
     prod = Product.find_by(id: params[:id])
-    render :plain, prod.inventory_available?
+    render plain: prod.inventory_available?
   end
 
   def index
