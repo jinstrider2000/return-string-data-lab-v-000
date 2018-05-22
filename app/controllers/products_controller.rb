d306310a3f6a706f670d1785705619b855135009
Efrain Perez Jr <efrainperezjr@live.com>
2018-05-22 20:44:25 +0000

    automatically backed up by learn

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,7 +24,7 @@ class ProductsController < ApplicationController
 
   def create
     Product.create(name: params[:name], description: params[:description], inventory: params[:inventory], price: params[:price])
-    redirect_to "index"
+    redirect_to products_path
   end
 
 end
